5d18bf952bd757947486b7c6e24c26855c351736
John Tan <tantai.john@gmail.com>
2015-03-04 16:49:30 -0800

    refactored to use defers

--- src/kvpaxos/state_machine.go
+++ src/kvpaxos/state_machine.go
@@ -3,23 +3,31 @@ package kvpaxos
 // This commit method is core of state machine.
 // This is only method that mutates core state.
 func (kv *KVPaxos) Commit(op Op, seq int) string {
-	kv.px.Done(seq)
+	defer kv.px.Done(seq)
+	// defer delete(kv.requests, op.Ack)
 	switch op.Op {
 	case "Put":
-		kv.data[op.Key] = op.Value
-		kv.requests[op.UID] = ""
-		delete(kv.requests, op.Ack)
+		if _, ok := kv.requests[op.UID]; !ok {
+			kv.data[op.Key] = op.Value
+			kv.requests[op.UID] = ""
+		}
 		return ""
 	case "Append":
-		previousValue := kv.data[op.Key]
-		kv.data[op.Key] += op.Value
-		kv.requests[op.UID] = previousValue
-		delete(kv.requests, op.Ack)
+		var previousValue string
+		if _, ok := kv.requests[op.UID]; !ok {
+			previousValue = kv.data[op.Key]
+			kv.data[op.Key] += op.Value
+			kv.requests[op.UID] = previousValue
+		} else {
+			previousValue = kv.requests[op.UID]
+		}
 		return previousValue
 	case "Get":
-		kv.requests[op.UID] = kv.data[op.Key]
-		delete(kv.requests, op.Ack)
-		return kv.data[op.Key]
+		if _, ok := kv.requests[op.UID]; !ok {
+			kv.requests[op.UID] = kv.data[op.Key]
+		}
+		value := kv.requests[op.UID]
+		return value
 	default:
 		return ""
 	}
